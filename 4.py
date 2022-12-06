3db3ce513001692b2e47bb8c245a80d10436363f
Saglara <nohaevass@yandex.ru>
2022-12-06 19:08:03 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,32 @@
-# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) 
+# многочлена и записать в файл многочлен степени k.
 
 # Пример:
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+k=int(input("Введите степень многочлена: "))
+koeff=[]
+for i in range(k+1):
+    koeff.append(randint(0,3))
+print(koeff)
+# koeff=[6, 5, 4, 3, 2, 1]
+data = open('многочлен.txt','a',encoding='UTF-8')
+
+for i in range(k+1):
+    if koeff[i]==1:#если коэффицинет=1, то не печатаем его
+        data.write("x^")
+        data.write(str(k-i))
+        data.write("+")
+    elif koeff[i]!=0:  #только если коэффициент не ноль, то печатаем его
+        if (k-i)!=0:  #проверка равен ли свободный член нулю
+            data.write(str(koeff[i]))
+            data.write("*x^")
+            data.write(str(k-i))
+            data.write("+")
+        else:
+            data.write(str(koeff[i]))
+data.write("=0")
+data.close()
+
