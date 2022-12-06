85447d8bd6be53466bca5d891226535212cb1094
Saglara <nohaevass@yandex.ru>
2022-12-06 14:51:32 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,2 +1,20 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+count=int(input("Введите количество последовательности чисел: "))
+from random import randint
+numbers=[]
+for i in range(count):
+    numbers.append(randint(0,10))
+print(numbers)
+numbers.sort()
+print(numbers)
+
+for i in range(0,len(numbers)):
+    x=0
+    for j in range(i+1,len(numbers)):
+        if numbers[i]==numbers[j]:
+            x=1
+    if x==0:
+        print(numbers[i])    
+
