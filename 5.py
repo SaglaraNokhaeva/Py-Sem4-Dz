3c2cda06dc92dca202e09c4f4756f78c28b41ef2
Saglara <nohaevass@yandex.ru>
2022-12-07 19:00:29 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,8 +1,10 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-data = open('многочлен1.txt','r',encoding='UTF-8')
-s1=data.readline()
-data1 = open('многочлен2.txt','r',encoding='UTF-8')
-s2=data1.readline()
+data1 = open('многочлен1.txt','r',encoding='UTF-8')
+s1=data1.readline()
+data2 = open('многочлен2.txt','r',encoding='UTF-8')
+s2=data2.readline()
+data1.close
+data2.close
 
