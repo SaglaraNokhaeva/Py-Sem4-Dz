b64a34861f3b33beb0feed2d69f1f0dfdcaa521b
Saglara <nohaevass@yandex.ru>
2022-12-10 12:22:32 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,9 +2,17 @@
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-d=input("Введите точность вычисления корня из 2: ")
-sqrt2=2**(1/2)
-d=d[2::]
-print(sqrt2)
-sqrt2=round(sqrt2,len(d))
-print(sqrt2)
+# d=input("Введите точность вычисления корня из 2: ")
+# sqrt2=2**(1/2)
+# d=d[2::]
+# print(sqrt2)
+# sqrt2=round(sqrt2,len(d))
+# print(sqrt2)
+
+
+# решение другое
+a = int(input('введите нужную точность 1#= '))
+pi_target = 0
+for i in range(1, 1000000):
+    pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+print(str(pi_target)[:a + 2])
